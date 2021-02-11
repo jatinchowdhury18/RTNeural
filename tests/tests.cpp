48585a71cb96d292ba1f127d81c2fb4da6adf7a4
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-02-11 14:54:32 -0500

    Build tests with std::map instead of std::unordered_map

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,7 +2,7 @@
 #include <RTNeural.h>
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <map>
 
 // @TODO: make tests for both float and double precision
 
@@ -15,17 +15,17 @@ struct TestConfig
     const double threshold;
 };
 
-static std::unordered_map<std::string, TestConfig> tests {
-    { "dense",
-        TestConfig { "DENSE", "models/dense.json", "test_data/dense_x_python.csv",
-            "test_data/dense_y_python.csv", 2.0e-8 } },
+static std::map<std::string, TestConfig> tests {
     { "conv1d",
         TestConfig { "CONV1D", "models/conv.json", "test_data/conv_x_python.csv",
             "test_data/conv_y_python.csv", 1.0e-6 } },
+    { "dense",
+        TestConfig { "DENSE", "models/dense.json", "test_data/dense_x_python.csv",
+            "test_data/dense_y_python.csv", 2.0e-8 } },
     { "gru", TestConfig { "GRU", "models/gru.json", "test_data/gru_x_python.csv", "test_data/gru_y_python.csv", 5.0e-6 } },
     { "lstm",
         TestConfig { "LSTM", "models/lstm.json", "test_data/lstm_x_python.csv",
-            "test_data/lstm_y_python.csv", 1.0e-6 } }
+            "test_data/lstm_y_python.csv", 1.0e-6 } },
 };
 
 void help()
