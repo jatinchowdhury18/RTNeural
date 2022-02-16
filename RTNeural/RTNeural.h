569422a5d759245f23d2473f383478df648a17e8
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-02-16 09:21:13 +0000

    Include <limits.h> in RTNeural header

--- RTNeural/RTNeural.h
+++ RTNeural/RTNeural.h
@@ -2,6 +2,10 @@
 
 // global include file for the RTNeural library!
 
+// C++ STL includes
+#include <limits.h>
+
+// RTNeural includes:
 #include "Model.h"
 #include "ModelT.h"
 #include "model_loader.h"
