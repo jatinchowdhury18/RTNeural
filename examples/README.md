cb577a271d718241f9a39f9d80a14c8cb501edab
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-04-16 19:26:40 +0100

    Add example for models with custom layers

--- examples/README.md
+++ examples/README.md
@@ -6,3 +6,4 @@ demonstrate usage of the RTNeural library:
 - `hello_rtneural`: Loads a model from a file and runs inference.
 - `rtneural_static_model`: Demonstrates how to use the RTNeural compile-time API to load and run a static model.
 - `rtneural_dynamic_model`: Demonstrates how to use the RTNeural run-time API to load and run a model from a file.
+- `custom_layer_model`: Demonstrates how to extend RTNeural's compile-time API with custom layers.
---
+++ examples/custom_layer_model/GatedActivation_STL.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <RTNeural/RTNeural.h>
+
+/** Static implementation of a custom gated activation layer. */
+template <typename T, int in_sizet>
+class GatedActivation
+{
+public:
+    static constexpr auto in_size = in_sizet;
+    static constexpr auto out_size = in_size / 2;
+
+    GatedActivation() = default;
+
+    /** Returns the name of this layer. */
+    std::string getName() const noexcept { return "gated_activation"; }
+
+    /**
+     * We're going to tell RTNeural to NOT treat this as an activation
+     * layer, since RTNeural expects activation layers to have the same
+     * inout/output sizes.
+     */
+    constexpr bool isActivation() const noexcept { return false; }
+
+    void reset() { }
+
+    /** Performs forward propagation for gated activation. */
+    inline void forward(const T (&ins)[in_size])
+    {
+        for(int i = 0; i < out_size; ++i)
+            outs[i] = std::tanh(ins[i]) * RTNeural::sigmoid(ins[i + out_size]);
+    }
+
+    T outs alignas(RTNEURAL_DEFAULT_ALIGNMENT)[out_size];
+};
