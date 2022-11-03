dcf774458e9dc8cd35f1d3b36953750bc1dbf770
Štěpán Miklánek <63413990+stepanmk@users.noreply.github.com>
2022-11-03 23:00:18 +0100

    Improve Conv1D layers (Eigen backend) (#74)
    * gitignore edit
    
    * edit gitignore & cmakelists
    
    * modify dynamic conv1d
    
    * edit func description
    
    * modify static conv1d layer
    
    * modify static conv1d
    
    * fix var names
    
    * fix static conv1d
    
    * gitignore edit
    
    * cmakelists edit
    
    * fix cmakelists & gitignore
    
    * fix clang formatting
    
    * gitignore edit
    
    * edit gitignore & cmakelists
    
    * modify dynamic conv1d
    
    * edit func description
    
    * modify static conv1d layer
    
    * modify static conv1d
    
    * fix var names
    
    * fix static conv1d
    
    * gitignore edit
    
    * cmakelists edit
    
    * fix cmakelists & gitignore
    
    * fix clang formatting
    
    * edit contributors

--- RTNeural/conv1d/conv1d_eigen.h
+++ RTNeural/conv1d/conv1d_eigen.h
@@ -10,7 +10,7 @@ namespace RTNeural
 /**
  * Dynamic implementation of a 1-dimensional convolution layer
  * with no activation.
- * 
+ *
  * This implementation was designed to be used for "temporal
  * convolution", so the layer has a "state" made up of past inputs
  * to the layer. To ensure that the state is initialized to zero,
@@ -23,7 +23,7 @@ class Conv1D : public Layer<T>
 public:
     /**
      * Constructs a convolution layer for the given dimensions.
-     * 
+     *
      * @param in_size: the input size for the layer
      * @param out_size: the output size for the layer
      * @param kernel_size: the size of the convolution kernel
@@ -44,32 +44,34 @@ public:
     /** Performs forward propagation for this layer. */
     inline void forward(const T* input, T* h) noexcept override
     {
-        inVec = Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>, RTNeuralEigenAlignment>(
-            input, Layer<T>::in_size, 1);
+        // insert input into a circular buffer
+        state.col(state_ptr) = Eigen::Map<const Eigen::Vector<T, Eigen::Dynamic>,
+            RTNeuralEigenAlignment>(input, Layer<T>::in_size);
 
-        // insert input into double-buffered state
-        state.col(state_ptr) = inVec;
-        state.col(state_ptr + state_size) = inVec;
+        // set state pointers to the particular columns of the buffer
+        setStatePointers();
 
+        // copy selected columns to a helper variable
+        for(int k = 0; k < kernel_size; ++k)
+            state_cols.col(k) = state.col(state_ptrs(k));
+
+        // perform a multichannel convolution
         for(int i = 0; i < Layer<T>::out_size; ++i)
-            outVec(i, 0) = state.block(0, state_ptr, Layer<T>::in_size, state_size).cwiseProduct(kernelWeights[i]).sum();
+            h[i] = state_cols.cwiseProduct(kernelWeights[i]).sum() + bias(i);
 
-        outVec = outVec + bias;
-        std::copy(outVec.data(), outVec.data() + Layer<T>::out_size, h);
-
-        state_ptr = (state_ptr == 0 ? state_size - 1 : state_ptr - 1); // iterate state pointer in reverse
+        state_ptr = (state_ptr == state_size - 1 ? 0 : state_ptr + 1); // iterate state pointer forwards
     }
 
     /**
      * Sets the layer weights.
-     * 
+     *
      * The weights vector must have size weights[out_size][in_size][kernel_size * dilation]
      */
     void setWeights(const std::vector<std::vector<std::vector<T>>>& weights);
 
     /**
      * Sets the layer biases.
-     * 
+     *
      * The bias vector must have size bias[out_size]
      */
     void setBias(const std::vector<T>& biasVals);
@@ -86,26 +88,35 @@ private:
     const int state_size;
 
     std::vector<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> kernelWeights;
-    Eigen::Matrix<T, Eigen::Dynamic, 1> bias;
+    Eigen::Vector<T, Eigen::Dynamic> bias;
 
     Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> state;
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> state_cols;
+    Eigen::VectorXi state_ptrs;
     int state_ptr = 0;
 
-    Eigen::Matrix<T, Eigen::Dynamic, 1> inVec;
-    Eigen::Matrix<T, Eigen::Dynamic, 1> outVec;
+    /** Sets pointers to state array columns. */
+    inline void setStatePointers()
+    {
+        for(int k = 0; k < kernel_size; ++k)
+        {
+            int r = (state_ptr - k * dilation_rate) % state_size;
+            state_ptrs(k) = r < 0 ? r + state_size : r;
+        }
+    }
 };
 
 //====================================================
 /**
  * Static implementation of a 1-dimensional convolution layer
  * with no activation.
- * 
+ *
  * This implementation was designed to be used for "temporal
  * convolution", so the layer has a "state" made up of past inputs
  * to the layer. To ensure that the state is initialized to zero,
  * please make sure to call `reset()` before your first call to
  * the `forward()` method.
- * 
+ *
  * @param in_sizet: the input size for the layer
  * @param out_sizet: the output size for the layer
  * @param kernel_size: the size of the convolution kernel
@@ -114,16 +125,18 @@ private:
 template <typename T, int in_sizet, int out_sizet, int kernel_size, int dilation_rate>
 class Conv1DT
 {
-    using vec_type = Eigen::Matrix<T, out_sizet, 1>;
+    using vec_type = Eigen::Vector<T, out_sizet>;
 
-    static constexpr auto state_size = kernel_size * dilation_rate;
-    using state_type = Eigen::Matrix<T, in_sizet, 2 * state_size>;
-
-    using weights_type = Eigen::Matrix<T, in_sizet, state_size>;
+    static constexpr auto state_size = (kernel_size - 1) * dilation_rate + 1;
+    using state_type = Eigen::Matrix<T, in_sizet, state_size>;
+    using weights_type = Eigen::Matrix<T, in_sizet, kernel_size>;
+    using state_ptrs_type = Eigen::Vector<int, kernel_size>;
 
 public:
     static constexpr auto in_size = in_sizet;
     static constexpr auto out_size = out_sizet;
+    static constexpr auto dilation = dilation_rate;
+    static constexpr auto kernel_length = kernel_size;
 
     Conv1DT();
 
@@ -139,28 +152,33 @@ public:
     /** Performs forward propagation for this layer. */
     inline void forward(const Eigen::Matrix<T, in_size, 1>& ins) noexcept
     {
-        // insert input into double-buffered state
+        // insert input into a circular buffer
         state.col(state_ptr) = ins;
-        state.col(state_ptr + state_size) = ins;
 
+        // set state pointers to particular columns of the buffer
+        setStatePointers();
+
+        // copy selected columns to a helper variable
+        for(int k = 0; k < kernel_length; ++k)
+            state_cols.col(k) = state.col(state_ptrs(k));
+
+        // perform a multichannel convolution
         for(int i = 0; i < out_size; ++i)
-            outs(i) = state.block(0, state_ptr, in_size, state_size).cwiseProduct(weights[i]).sum();
+            outs(i) = state_cols.cwiseProduct(weights[i]).sum() + bias(i);
 
-        outs = outs + bias;
-
-        state_ptr = (state_ptr == 0 ? state_size - 1 : state_ptr - 1); // iterate state pointer in reverse
+        state_ptr = (state_ptr == state_size - 1 ? 0 : state_ptr + 1); // iterate state pointer forwards
     }
 
     /**
      * Sets the layer weights.
-     * 
+     *
      * The weights vector must have size weights[out_size][in_size][kernel_size * dilation]
      */
     void setWeights(const std::vector<std::vector<std::vector<T>>>& weights);
 
     /**
      * Sets the layer biases.
-     * 
+     *
      * The bias vector must have size bias[out_size]
      */
     void setBias(const std::vector<T>& biasVals);
@@ -177,10 +195,23 @@ private:
     T outs_internal alignas(RTNEURAL_DEFAULT_ALIGNMENT)[out_size];
 
     state_type state;
+    weights_type state_cols;
+
     int state_ptr = 0;
+    state_ptrs_type state_ptrs;
 
     weights_type weights[out_size];
     vec_type bias;
+
+    /** Sets pointers to state array columns. */
+    inline void setStatePointers()
+    {
+        for(int k = 0; k < kernel_length; ++k)
+        {
+            int r = (state_ptr - k * dilation) % state_size;
+            state_ptrs(k) = r < 0 ? r + state_size : r;
+        }
+    }
 };
 
 } // RTNeural
