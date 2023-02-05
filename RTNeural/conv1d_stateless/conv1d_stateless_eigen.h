5df7e7175b2d4545aa0596187fd221fe7dc9486c
Damien Ronssin <damien.ronssin@gmail.com>
2023-02-05 14:49:34 +0100

    Dynamic implementation of conv1d_stateless and added use_bias as template parameter

--- RTNeural/conv1d_stateless/conv1d_stateless_eigen.h
+++ RTNeural/conv1d_stateless/conv1d_stateless_eigen.h
@@ -7,18 +7,91 @@
 
 namespace RTNeural
 {
+/**
+ * Dynamic implementation of a 1-dimensional stateless convolution layer with no activation.
+ * This implementation was designed to be used for a single frame of features, fully available at each forward call.
+ * So the layer has a NO internal "state"
+ *
+ * @tparam T Type of the layer (float, double, int ...)
+ * @tparam use_bias Whether this layer has a bias
+ */
+template <typename T, bool use_bias = true>
+class Conv1DStateless : public Layer<T>
+{
+public:
+    Conv1DStateless(int in_num_filters_in, int in_num_features_in, int in_num_filters_out, int in_kernel_size, int in_stride);
+    Conv1DStateless(std::initializer_list<int> sizes);
+    Conv1DStateless(const Conv1DStateless& other);
+    Conv1DStateless& operator=(const Conv1DStateless& other);
+    virtual ~Conv1DStateless() = default;
 
-// enum Conv1DStatelessPaddingType
-//{
-//     Valid = 0,
-//     Same
-// };
+    /** Resets the layer state. */
+    void reset() override {};
+
+    /** Returns the name of this layer. */
+    std::string getName() const noexcept { return "conv1d_stateless"; }
+
+    /** Returns false since convolution is not an activation layer. */
+    constexpr bool isActivation() const noexcept { return false; }
+
+    /** Performs forward propagation for this layer. */
+    inline void forward(const T* input, T* output) noexcept override
+    {
+        auto inMatrix = Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>,
+            RTNeuralEigenAlignment>(input, num_filters_in, num_features_in);
+
+        auto outMatrix = Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>,
+            RTNeuralEigenAlignment>(output, num_filters_out, num_features_out);
+
+        // perform a multichannel convolution
+        for(int i = 0; i < num_filters_out; i++)
+        {
+            for(int j = 0; j < num_features_out; j++)
+            {
+                if (use_bias)
+                    outMatrix(i, j) = kernelWeights[i].cwiseProduct(inMatrix.middleCols(j * stride, kernel_size)).sum() + bias(i);
+                else
+                    outMatrix(i, j) = kernelWeights[i].cwiseProduct(inMatrix.middleCols(j * stride, kernel_size)).sum();
+            }
+        }
+    }
+
+    /**
+     * Sets the layer weights.
+     *
+     * The weights vector must have size weights[num_filters_out][num_filters_in][kernel_size]
+     */
+    void setWeights(const std::vector<std::vector<std::vector<T>>>& inWeights);
+
+    /**
+     * Sets the layer biases.
+     *
+     * The bias vector must have size bias[num_filters_out]
+     */
+    void setBias(const std::vector<T>& inBias);
+
+    /** Returns the size of the convolution kernel. */
+    int getKernelSize() const noexcept { return kernel_size; }
+
+    /** Returns the stride. */
+    int getStride() const noexcept { return stride; }
+
+private:
+    const int num_filters_in;
+    const int num_features_in;
+    const int num_filters_out;
+    const int kernel_size;
+    const int stride;
+    const int num_features_out = (num_features_in - kernel_size) / stride + 1;
+
+    std::vector<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> kernelWeights;
+    Eigen::Vector<T, Eigen::Dynamic> bias;
+};
 
 //====================================================
 
 /**
- * Static implementation of a 1-dimensional stateless convolution layer
- * with no activation.
+ * Static implementation of a 1-dimensional stateless convolution layer with no activation.
  * This implementation was designed to be used for a single frame of features, fully available at each forward call.
  * So the layer has a NO internal "state"
  *
@@ -29,14 +102,14 @@ namespace RTNeural
  * @tparam kernel_size size of the convolution kernel
  * @tparam stride convolution stride
  */
-template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size, int stride>
+template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size,
+    int stride, bool use_bias = true>
 class Conv1DStatelessT
 {
-    // TODO: notify that Same padding with stride != 1 is not possible.
-    using bias_type = Eigen::Vector<T, num_filters_out>;
+    using bias_type = Eigen::Vector<T, use_bias ? num_filters_out : 0>;
     using weights_type = Eigen::Matrix<T, num_filters_in, kernel_size>;
     using input_type = Eigen::Matrix<T, num_filters_in, num_features_in>;
-    static constexpr int num_features_out = (num_features_in - kernel_size) / stride + 1; // TODO: to check
+    static constexpr int num_features_out = (num_features_in - kernel_size) / stride + 1; // TODO: to test
     using output_type = Eigen::Matrix<T, num_filters_out, num_features_out>;
 
 public:
@@ -60,7 +133,10 @@ public:
             for(int j = 0; j < num_features_out; j++)
             {
                 // TODO: manage to use middleCols<kernel_size>(j*stride)
-                outs(i, j) = weights[i].cwiseProduct(inMatrix.middleCols(j*stride, kernel_size)).sum() + bias(i);
+                if (use_bias)
+                    outs(i, j) = weights[i].cwiseProduct(inMatrix.middleCols(j * stride, kernel_size)).sum() + bias(i);
+                else
+                    outs(i, j) = weights[i].cwiseProduct(inMatrix.middleCols(j * stride, kernel_size)).sum();
             }
         }
     }
--- RTNeural/conv1d_stateless/conv1d_stateless_eigen.tpp
+++ RTNeural/conv1d_stateless/conv1d_stateless_eigen.tpp
@@ -2,19 +2,74 @@
 
 namespace RTNeural
 {
+template <typename T, bool use_bias>
+Conv1DStateless<T, use_bias>::Conv1DStateless(int in_num_filters_in, int in_num_features_in, int in_num_filters_out, int in_kernel_size, int in_stride)
+    : num_filters_in(in_num_filters_in)
+    , num_features_in(in_num_features_in)
+    , num_filters_out(in_num_filters_out)
+    , kernel_size(in_kernel_size)
+    , stride(in_stride)
+{
+    kernelWeights.resize(num_filters_out);
+    for(int i = 0; i < num_filters_out; ++i)
+        kernelWeights[i] = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Zero(num_features_in, kernel_size);
 
-template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size, int stride>
-Conv1DStatelessT<T, num_filters_in, num_features_in, num_filters_out, kernel_size, stride>::Conv1DStatelessT()
+    if (use_bias)
+        bias = Eigen::Vector<T, Eigen::Dynamic>::Zero(num_filters_out);
+}
+
+template <typename T, bool use_bias>
+Conv1DStateless<T, use_bias>::Conv1DStateless(std::initializer_list<int> sizes)
+    : Conv1DStateless<T, use_bias>(*sizes.begin(), *(sizes.begin() + 1), *(sizes.begin() + 2), *(sizes.begin() + 3), *(sizes.begin() + 4))
+{
+}
+
+template <typename T, bool use_bias>
+Conv1DStateless<T, use_bias>::Conv1DStateless(const Conv1DStateless& other)
+    : Conv1DStateless(other.num_filters_in, other.num_features_in, other.num_filters_out, other.kernel_size, other.stride)
+{
+}
+
+template <typename T, bool use_bias>
+Conv1DStateless<T, use_bias>& Conv1DStateless<T, use_bias>::operator=(const Conv1DStateless<T, use_bias>& other)
+{
+    return *this = Conv1DStateless<T>(other);
+}
+
+template <typename T, bool use_bias>
+void Conv1DStateless<T, use_bias>::setWeights(const std::vector<std::vector<std::vector<T>>>& inWeights)
+{
+    for(int i = 0; i < Layer<T>::out_size; ++i)
+        for(int k = 0; k < Layer<T>::in_size; ++k)
+            for(int j = 0; j < kernel_size; ++j)
+                kernelWeights[i](k, j) = inWeights[i][k][j];
+}
+
+template <typename T, bool use_bias>
+void Conv1DStateless<T, use_bias>::setBias(const std::vector<T>& inBias)
+{
+    if(use_bias)
+    {
+        for(int i = 0; i < Layer<T>::out_size; ++i)
+            bias(i) = inBias[i];
+    }
+}
+
+//====================================================
+
+template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size, int stride, bool use_bias>
+Conv1DStatelessT<T, num_filters_in, num_features_in, num_filters_out, kernel_size, stride, use_bias>::Conv1DStatelessT()
     : outs(outs_internal)
 {
     for(int k = 0; k < num_filters_out; ++k)
         weights[k] = weights_type::Zero();
 
-    bias = bias_type::Zero();
+    if(use_bias)
+        bias = bias_type::Zero();
 }
 
-template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size, int stride>
-void Conv1DStatelessT<T, num_filters_in, num_features_in, num_filters_out, kernel_size, stride>::setWeights(const std::vector<std::vector<std::vector<T>>>& inWeights)
+template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size, int stride, bool use_bias>
+void Conv1DStatelessT<T, num_filters_in, num_features_in, num_filters_out, kernel_size, stride, use_bias>::setWeights(const std::vector<std::vector<std::vector<T>>>& inWeights)
 {
     for(int i = 0; i < num_features_out; ++i)
         for(int k = 0; k < num_features_in; ++k)
@@ -22,10 +77,11 @@ void Conv1DStatelessT<T, num_filters_in, num_features_in, num_filters_out, kerne
                 weights[i](k, j) = inWeights[i][k][j];
 }
 
-template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size, int stride>
-void Conv1DStatelessT<T, num_filters_in, num_features_in, num_filters_out, kernel_size, stride>::setBias(const std::vector<T>& inBias)
+template <typename T, int num_filters_in, int num_features_in, int num_filters_out, int kernel_size, int stride, bool use_bias>
+void Conv1DStatelessT<T, num_filters_in, num_features_in, num_filters_out, kernel_size, stride, use_bias>::setBias(const std::vector<T>& inBias)
 {
-    for(int i = 0; i < num_features_out; ++i)
-        bias(i) = inBias[i];
+    if(use_bias)
+        for(int i = 0; i < num_features_out; ++i)
+            bias(i) = inBias[i];
 }
 } // RTNeural
