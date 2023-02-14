329d2cb22c09edf93ba8f45bc1b9da48053e1d4a
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2023-02-14 11:59:03 -0800

    Fix documentation

--- RTNeural/conv1d/conv1d_eigen.h
+++ RTNeural/conv1d/conv1d_eigen.h
@@ -118,6 +118,7 @@ private:
  * @param out_sizet: the output size for the layer
  * @param kernel_size: the size of the convolution kernel
  * @param dilation_rate: the dilation rate to use for dilated convolution
+ * @param dynamic_state: use dynamically allocated layer state
  */
 template <typename T, int in_sizet, int out_sizet, int kernel_size, int dilation_rate, bool dynamic_state = false>
 class Conv1DT
