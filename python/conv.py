2978f641e4129501ee56c8294a9fa46bce770b43
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2023-05-01 11:08:02 -0700

    Fixes and optimizations for XSIMD Conv1D implementation (#101)
    * Fixes and optimizations for XSIMD Conv1D implementation
    
    * Apply clang-format
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- python/conv.py
+++ python/conv.py
@@ -16,11 +16,11 @@ model.add(keras.layers.Dense(8, activation='tanh', kernel_initializer='random_no
 model.add(keras.layers.Conv1D(4, 3, dilation_rate=1, activation='tanh', padding='causal', kernel_initializer='glorot_uniform', bias_initializer='random_normal'))
 model.add(keras.layers.BatchNormalization(momentum=0.0, epsilon=0.01, beta_initializer='random_normal', gamma_initializer='glorot_uniform', moving_mean_initializer="random_normal", moving_variance_initializer="ones"))
 model.add(keras.layers.PReLU(alpha_initializer='glorot_uniform', shared_axes=[1]))
+model.add(keras.layers.Conv1D(4, 1, dilation_rate=1, activation='tanh', padding='causal', kernel_initializer='glorot_uniform', bias_initializer='random_normal'))
 model.add(keras.layers.Conv1D(4, 3, dilation_rate=2, activation='tanh', padding='causal', kernel_initializer='glorot_uniform', bias_initializer='random_normal'))
 model.add(keras.layers.BatchNormalization(center=False, scale=False, momentum=0.0, epsilon=5.0, moving_mean_initializer="random_normal", moving_variance_initializer="random_uniform")) # similar to PyTorch "affine" layer
 model.add(keras.layers.PReLU(alpha_initializer='glorot_uniform', shared_axes=[0,1]))
-model.add(keras.layers.Dense(8, activation='sigmoid', kernel_initializer=tf.keras.initializers.Orthogonal(), bias_initializer='random_normal'))
-model.add(keras.layers.Dense(1, kernel_initializer='orthogonal', bias_initializer='random_normal'))
+model.add(keras.layers.Dense(1, activation='sigmoid', kernel_initializer=tf.keras.initializers.Orthogonal(), bias_initializer='random_normal'))
 
 # construct signals
 x = 10 * np.sin(np.arange(N) * np.pi * 0.1)
