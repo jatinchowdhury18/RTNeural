f4fa2f0a52724b5969393f649a42c3d4b618556f
purefunctor <purefunctor@pm.me>
2023-11-15 03:46:09 +0800

    Fix data generation script

--- python/conv1d_torch_group.py
+++ python/conv1d_torch_group.py
@@ -3,11 +3,14 @@ import numpy as np
 import torch
 from torch import nn
 
-f = nn.Conv1d(in_channels=6, out_channels=3, kernel_size=1, groups=3)
-x = torch.from_numpy(np.random.uniform(-1, 1, 3000).astype("float32")).reshape(1, 6, -1)
+np.random.seed(1001)
+torch.manual_seed(0)
+
+f = nn.Conv1d(in_channels=6, out_channels=3, kernel_size=1, groups=3, dtype=torch.float64)
+x = torch.from_numpy(np.random.uniform(-1, 1, 3000)).reshape(1, 6, -1)
 y = f(x).detach().numpy()
 
-np.savetxt("test_data/conv1d_torch_group_x_python.csv", x[0], delimiter=",")
+np.savetxt("test_data/conv1d_torch_group_x_python.csv", x[0].T, delimiter=",")
 np.savetxt("test_data/conv1d_torch_group_y_python.csv", y[0], delimiter=",")
 
 class EncodeTensor(json.JSONEncoder):
